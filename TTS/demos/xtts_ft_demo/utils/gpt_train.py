ec305c24390569d2a87f2600988833789c3fc8ec
mphuc02 <brolydinh@gmail.com>
2024-07-30 21:30:12 +0700

    fixed bugs: - Fixed wrong config.json link

--- TTS/demos/xtts_ft_demo/utils/gpt_train.py
+++ TTS/demos/xtts_ft_demo/utils/gpt_train.py
@@ -63,8 +63,8 @@ def train_gpt(language, num_epochs, batch_size, grad_acumm, train_csv, eval_csv,
     #TOKENIZER_FILE_LINK = "https://coqui.gateway.scarf.sh/hf-coqui/XTTS-v2/main/vocab.json"
 
     XTTS_CHECKPOINT_LINK = "https://coqui.gateway.scarf.sh/hf-coqui/XTTS-v2/main/model.pth"
-
-    XTTS_CONFIG_LINK = "https://huggingface.co/capleaf/viXTTS/blob/main/config.json"
+            
+    XTTS_CONFIG_LINK = "https://huggingface.co/capleaf/viXTTS/resolve/main/config.json"
     #XTTS_CONFIG_LINK = "https://coqui.gateway.scarf.sh/hf-coqui/XTTS-v2/main/config.json"
 
     # XTTS transfer learning parameters: You we need to provide the paths of XTTS model checkpoint that you want to do the fine tuning.
